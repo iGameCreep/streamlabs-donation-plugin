f426c1eba9aa60f4f14c0cb3dc90b16bd1440922
iGameCreep <gamecreep35@gmail.com>
2025-04-05 11:37:45 +0200

    WIP - Back to Socket.IO

--- src/main/java/fr/gamecreep/fundraiserfusion/websocket/StreamlabsWebSocketClient.java
+++ src/main/java/fr/gamecreep/fundraiserfusion/websocket/StreamlabsWebSocketClient.java
@@ -1,25 +1,25 @@
 package fr.gamecreep.fundraiserfusion.websocket;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import fr.gamecreep.fundraiserfusion.FundraiserFusion;
 import fr.gamecreep.fundraiserfusion.donations.entities.Donation;
 import fr.gamecreep.fundraiserfusion.donations.entities.api.DonationEvent;
 import org.bukkit.Bukkit;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.engineio.client.transports.WebSocket;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
 public class StreamlabsWebSocketClient {
 
-    //TODO: USE PROD ENDPOINT
-    //private static final String WEBSOCKET_ENDPOINT = "wss://sockets.streamlabs.com";
-    private static final String WEBSOCKET_ENDPOINT = "ws://localhost:8080";
+    private static final String WEBSOCKET_ENDPOINT = "https://sockets.streamlabs.com";
 
     private final Gson gson = new Gson();
     private final FundraiserFusion plugin;
-    private WebSocketClient webSocketClient;
+    private Socket socket = null;
 
     public StreamlabsWebSocketClient(final FundraiserFusion plugin) {
         this.plugin = plugin;
@@ -32,40 +32,52 @@ public class StreamlabsWebSocketClient {
 
     private void loadWebSocket(final String socketToken) {
         try {
-            final URI uri = new URI(WEBSOCKET_ENDPOINT + "?token=" + socketToken);
+            final IO.Options options = new IO.Options();
+            options.transports = new String[]{WebSocket.NAME};
+            options.query = "token=" + socketToken;
 
-            this.webSocketClient = new WebSocketClient(uri) {
-                @Override
-                public void onOpen(final ServerHandshake handshakedata) {
-                    plugin.getLogger().info("Loaded websocket");
-                }
+            this.socket = IO.socket(WEBSOCKET_ENDPOINT, options);
 
-                @Override
-                public void onMessage(final String message) {
-                    final DonationEvent event = gson.fromJson(message, DonationEvent.class);
-                    handleDonationEvent(event);
-                }
+            socket.on(Socket.EVENT_CONNECT, args -> this.plugin.getLogger().info("Loaded websocket"));
 
-                @Override
-                public void onClose(final int code, final String reason, final boolean remote) {
-                    plugin.getLogger().info("WebSocket closed: " + reason);
-                }
+            socket.on("event", this::onSocketEvent);
 
-                @Override
-                public void onError(final Exception ex) {
-                    plugin.getLogger().warning("WebSocket error: " + ex.getMessage());
-                }
-            };
-
-            this.webSocketClient.connect();
+            socket.connect();
         } catch (URISyntaxException e) {
             this.plugin.getLogger().warning("Unable to load the websocket.");
         }
     }
 
     public void endWebSocket() {
-        if (this.webSocketClient != null && this.webSocketClient.isOpen()) {
-            this.webSocketClient.close();
+        if (this.socket != null && this.socket.isActive()) {
+            this.socket.close();
+            this.plugin.getLogger().info("WebSocket closed successfully !");
+        } else {
+            this.plugin.getLogger().warning("Unable to close WebSocket.");
+        }
+    }
+
+    private void onSocketEvent(final Object[] args) {
+        try {
+            if (args.length > 0 && args[0] != null) {
+                // API docs: https://dev.streamlabs.com/docs/socket-api
+                final JsonObject rawEvent = JsonParser.parseString(args[0].toString()).getAsJsonObject();
+                final String type = rawEvent.get("type").getAsString();
+
+                if (type == null) {
+                    return;
+                }
+
+                if (type.equals("donation")) {
+                    DonationEvent event = gson.fromJson(args[0].toString(), DonationEvent.class);
+                    handleDonationEvent(event);
+                } else {
+                    this.plugin.getLogger().info("Ignoring event: " + type);
+                }
+            }
+        } catch (final Exception e) {
+            this.plugin.getLogger().severe("Unable to parse event from StreamLabs WS: " + e.getMessage());
+            this.endWebSocket();
         }
     }
 
